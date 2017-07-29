f8b37b57443c6fa4b2569ffae506874a5a50bcdd
Tatsuya Otsuka <tatzyr@gmail.com>
2017-07-30 00:25:19 +0900

    Move main loop and delete main

--- tut_lecture_bot.rb
+++ tut_lecture_bot.rb
@@ -1,3 +1,36 @@
 #/usr/bin/env ruby
 
 require_relative "lib/tut_lecture_bot"
+require "logger"
+
+logger = Logger.new("log/tut_lecture_bot.log", "daily")
+logger.info("start")
+
+tweet_composer = TUTLectureBot::TweetComposer.new(YAML.load_file("settings.yml"))
+
+canceled_lectures, extra_lectures = nil, nil
+
+logger.info("accessing")
+begin
+  agent = TUTLectureBot::Agent.new
+  canceled_lectures = agent.lectures(:canceled)
+  extra_lectures = agent.lectures(:extra)
+rescue StandardError => e
+  logger.error(e.inspect)
+else
+  canceled_lectures.each do |canceled_lecture|
+    tweet_composer.tweet(canceled_lecture, :canceled) do |loginfo|
+      logger.info(loginfo)
+      sleep(10)
+    end
+  end
+
+  extra_lectures.each do |extra_lecture|
+    tweet_composer.tweet(extra_lecture, :extra) do |loginfo|
+      logger.info(loginfo)
+      sleep(10)
+    end
+  end
+end
+
+logger.info("sleeping")
