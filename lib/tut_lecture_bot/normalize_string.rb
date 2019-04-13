424414986e773ae1a728b5b8eb206dee0e900b2c
Tatsuya Otsuka <tatzyr@gmail.com>
2019-04-13 17:45:54 +0900

    Change department name

--- lib/tut_lecture_bot/normalize_string.rb
+++ lib/tut_lecture_bot/normalize_string.rb
@@ -4,7 +4,7 @@ module TUTLectureBot
       "機械" => "1系", # Mechanical Engineering
       "電気・電子情" => "2系", # Electrical and Electronic Information Engineering
       "情報・知能" => "3系", # Computer Science and Engineering
-      "環境・生命" => "4系", # Environmental and Life Sciences
+      "応用・生命" => "4系", # Applied Chemistry and Life Science
       "建築・都市" => "5系", # Architecture and Civil Engineering
       "共通" => "共通", # All Departments
       "電気電子情報" => "2系", # F---
